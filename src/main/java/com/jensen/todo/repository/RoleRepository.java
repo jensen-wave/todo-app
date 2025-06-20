ab0eac9c2c14ded8fbdf3c937aefc95826d67eda
jensen-wave <chssun@gmail.com>
2025-06-20 23:10:09 +0800

    建立 User 與 Role JPA 實體並實作多對多關聯，完成對應 Repository 與自訂查詢方法

---
+++ src/main/java/com/jensen/todo/repository/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.jensen.todo.repository;
+
+import com.jensen.todo.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role,Long> {
+}
---
+++ src/main/java/com/jensen/todo/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.jensen.todo.repository;
+
+import com.jensen.todo.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+
+    Optional<User> findByUsername(String username);
+    Boolean existsByEmail(String email);
+    Optional<User> findByUsernameOrEmail(String username,String email);
+}
